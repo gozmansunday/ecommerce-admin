5d5c897d9bbd3645c194f03f4e829e8ae30dbd7f
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-02-05 02:56:52 +0100

    feat: create modal provider

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+// Global Imports
+import { useEffect, useState } from "react";
+
+// Local Imports
+import { StoreModal } from "@/components/modals/store-modal";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return <StoreModal />;
+};
