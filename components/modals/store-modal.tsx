af24732660340efa80d84dd1d6e26c176bce6f39
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-02-05 03:50:09 +0100

    refactor: update store modal

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,33 @@
 "use client";
 
+// Global Imports
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+// Local Imports
 import { useStoreModal } from "@/hooks/useStoreModal";
+import { createStoreSchema } from "@/models/zodSchemas";
 import { Modal } from "../ui/modal";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  // Zod Form Validator
+  const form = useForm<z.infer<typeof createStoreSchema>>({
+    resolver: zodResolver(createStoreSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof createStoreSchema>) => {
+    console.log(values);
+    // TODO: Create new store
+  };
 
   return (
     <Modal
@@ -13,7 +36,43 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Store modal for creating new shit!
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          {/* Store Name */}
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Enter the name of the new store"
+                    disabled={form.formState.isSubmitting}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <section className="flex flex-col-reverse gap-2 mt-6 md:flex-row md:items-center md:justify-end">
+            <Button
+              onClick={storeModal.onClose}
+              variant={"outline"}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+            >
+              Continue
+            </Button>
+          </section>
+        </form>
+      </Form>
     </Modal>
   );
 };
