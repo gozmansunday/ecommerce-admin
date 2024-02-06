d83d47e36b3ef5a9a66e497c0fca841e8f43ad87
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-02-06 18:50:24 +0100

    refactor: update zod schema

--- models/zodSchemas.ts
+++ models/zodSchemas.ts
@@ -4,5 +4,5 @@ import { z } from "zod";
 export const createStoreSchema = z.object({
   name: z
     .string().trim()
-    .min(1, { message: "Song title is required" }),
+    .min(1, { message: "Store name is required" }),
 });
