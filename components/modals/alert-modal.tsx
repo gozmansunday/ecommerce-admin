d417508aad5d253886059f24873c7f8a4ae724da
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-02-10 03:03:31 +0100

    feat: create alert modal

---
+++ components/modals/alert-modal.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+// Global Imports
+import { useEffect, useState } from "react";
+import { TbLoader } from "react-icons/tb";
+
+// Local Imports
+import { Modal } from "../ui/modal";
+import { Button } from "../ui/button";
+
+interface Props {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  isPending: boolean;
+};
+
+export const AlertModal = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  onConfirm,
+  isPending
+}: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Modal
+      title={title}
+      description={description}
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <section className="flex flex-col-reverse gap-2 mt-6 md:flex-row md:items-center md:justify-end">
+        <Button
+          variant={"outline"}
+          onClick={onClose}
+          disabled={isPending}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant={"destructive"}
+          onClick={onConfirm}
+          disabled={isPending}
+        >
+          {isPending ?
+            <TbLoader
+              size={24}
+              className="animate-spin"
+            /> :
+            <span>Continue</span>}
+        </Button>
+      </section>
+    </Modal>
+  );
+};
