3a7372eecc3064362363b16e90d32a8c5043c989
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-03-15 13:48:58 +0100

    refactor: update api

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -19,6 +19,9 @@ export async function GET(
       where: {
         id: categoryId,
       },
+      include: {
+        billboard: true,
+      }
     });
 
     return NextResponse.json(category);
